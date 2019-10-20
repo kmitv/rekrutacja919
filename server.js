f93f141f17e56626dd47f3ae5fb883ab24cac844
kmitv <wojtek.kmita@hotmail.com>
2019-10-20 21:50:14 +0200

    small fix

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/blog-front-end'));
+app.use(express.static(__dirname + '/dist/rekrutacja919'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/blog-front-end/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/rekrutacja919/index.html'));
 });
 
 // default Heroku PORT
